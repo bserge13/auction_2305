b2e2cefd99079a46ce5d35f3ee2d05d74fa096f9
Blake Sergesketter <blakesergesketter@gmail.com>
2023-06-14 10:30:57 -0500

    complete all iteration 1 class methods, and interaction patterns.

--- lib/auction.rb
+++ lib/auction.rb
@@ -4,4 +4,16 @@ class Auction
   def initialize
     @items = []
   end
+
+  def add_item(item)
+    @items << item 
+  end
+
+  def item_names
+    names = []
+    @items.each do |item|
+      names << item.name
+    end
+    names 
+  end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -3,5 +3,20 @@ require './lib/auction'
 require './lib/attendee'
 
 RSpec.describe Auction do 
+  it 'initializes with attributes' do 
+    auction = Auction.new
+    expect(auction.items).to eq([])
+  end
+
+  it 'can add items to empty array' do 
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    auction.add_item(item1)
+    auction.add_item(item2)
+    expect(auction.items).to eq([item1, item2])
+    expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+  end
+
   
 end
